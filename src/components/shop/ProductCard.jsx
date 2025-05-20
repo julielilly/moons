98719219a073b0dfc3ec9917111706ff1c79a4e0
Julie <julieglion@gmail.com>
2025-05-20 14:26:11 +0200

    fix error 'Links do not have a discernible name'

--- src/components/shop/ProductCard.jsx
+++ src/components/shop/ProductCard.jsx
@@ -22,58 +22,60 @@ const ProductCard = ({ product }) => {
     <article className="product-card group relative">
       <Link
         href={`/shop/${product.handle}`}
-        className="group absolute bottom-0 left-0 right-0 top-0"
-      />
-
-      {/* nyhed */}
-      {newProduct && !onSale && (
-        <div className={`${tag} rounded-e-sm`}>Nyhed</div>
-      )}
-
-      {/* udsalg */}
-      {onSale && (
-        <div className={`${tag} text-background rounded-e-sm bg-red-800`}>
-          Tilbud
-        </div>
-      )}
-
-      {/* udsolgt */}
-      {soldOut && <div className={`${tag} right-0 rounded-s-sm`}>Udsolgt</div>}
-
-      <div className="image-container aspect-4/5 mb-2 w-full overflow-hidden rounded-lg">
-        <Image
-          src={product.images.edges[0]?.node.url}
-          alt={product.title}
-          width={300}
-          height={300}
-          className="h-full w-full object-cover"
-        />
-      </div>
-
-      <div className={`${soldOut ? "text-dark-grey" : ""}`}>
-        <p className="font-h text-body-lg">{product.title}</p>
-        <p className="text-caption m-0">
-          {new Intl.NumberFormat("da-DK", {
-            style: "currency",
-            currency: "DKK",
-          }).format(product.variants.edges[0].node.price.amount)}
-        </p>
-        {onSale && (
-          <p className="m-0 text-[0.75rem]">
-            <span className="text-dark-grey line-through">
-              {new Intl.NumberFormat("da-DK", {
-                style: "currency",
-                currency: "DKK",
-              }).format(onSale.amount)}
-            </span>
-            <span
-              className={`${soldOut ? "text-dark-grey" : "text-red-800"} no-line-through ml-2xs`}
-            >
-              -${discountPercent}%
-            </span>
-          </p>
+        className="group font-normal hover:no-underline"
+      >
+        {/* nyhed */}
+        {newProduct && !onSale && (
+          <div className={`${tag} rounded-e-sm`}>Nyhed</div>
         )}
-      </div>
+
+        {/* udsalg */}
+        {onSale && (
+          <div className={`${tag} text-background rounded-e-sm bg-red-800`}>
+            Tilbud
+          </div>
+        )}
+
+        {/* udsolgt */}
+        {soldOut && (
+          <div className={`${tag} right-0 rounded-s-sm`}>Udsolgt</div>
+        )}
+
+        <div className="image-container aspect-4/5 mb-2 w-full overflow-hidden rounded-lg">
+          <Image
+            src={product.images.edges[0]?.node.url}
+            alt={product.title}
+            width={300}
+            height={300}
+            className="h-full w-full object-cover"
+          />
+        </div>
+
+        <div className={`${soldOut ? "text-dark-grey" : ""}`}>
+          <p className="font-h text-body-lg">{product.title}</p>
+          <p className="text-caption m-0">
+            {new Intl.NumberFormat("da-DK", {
+              style: "currency",
+              currency: "DKK",
+            }).format(product.variants.edges[0].node.price.amount)}
+          </p>
+          {onSale && (
+            <p className="m-0 text-[0.75rem]">
+              <span className="text-dark-grey line-through">
+                {new Intl.NumberFormat("da-DK", {
+                  style: "currency",
+                  currency: "DKK",
+                }).format(onSale.amount)}
+              </span>
+              <span
+                className={`${soldOut ? "text-dark-grey" : "text-red-800"} no-line-through ml-2xs`}
+              >
+                -${discountPercent}%
+              </span>
+            </p>
+          )}
+        </div>
+      </Link>
     </article>
   );
 };
